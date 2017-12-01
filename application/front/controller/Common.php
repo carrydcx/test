cb923af93c2845a11355c027984fda0026b38b09
dichangxue <996631042@qq.com>
2017-12-01 17:08:21 +0800

    1

---
+++ application/front/controller/Common.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app\judanbao\controller;
+
+class Common {
+
+    
+        
+}
---
+++ application/front/controller/Index.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\front\controller;
+
+class Index extends CommonController
+{
+    public function index()
+    {
+
+    }
+
+    public function login(){
+        
+    }
+}
